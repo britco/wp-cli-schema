09fe8f31e85ac69d50a7a06589e8e7d543096794
Paul Dufour <paul.t.dufour@gmail.com>
2015-08-11 17:53:13 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+## wp-cli-schema
+
+Adds extra commands to [wp-cli](http://wp-cli.org/) to do schema upgrades for
+Wordpress. It allows you to do DB upgrades on deploy, instead of having to do
+hacks like re-activating your plugin so it fires activation hooks again.
+
+Also you don't have to worry about the PHP process timing out since it's
+executed in the CLI context and not the browser.
+
+## Usage
+
+In your plugin, add a hook for the `schema_upgrade` action. It should behave
+similar to https://codex.wordpress.org/Creating_Tables_with_Plugins. It should
+
+````
+add_action('schema_upgrade', function() {
+  if(!term_exists('Category', 'segment')) {
+    wp_insert_term('Category', 'segment');
+  }
+});
+````
+
+**Add the schema upgrade to your deploy process**
+
+````
+wp schema upgrade
+````
+
+When this runs, it will essentially call `do_action('schema_upgrade')`.
+
+## License
+Available under the MIT License.
---
+++ wp-cli-schema.php
@@ -0,0 +1,92 @@
+<?php
+namespace WP_CLI_Schema;
+
+use WP_CLI_Command, WP_CLI, ReflectionMethod, ReflectionFunction;
+
+/*
+Plugin Name: WP-CLI Schema
+Plugin URI:
+Description: Adds 'schema' commands to wp-cli
+Author: Paul Dufour
+Version: 0.1
+Author URI: http://www.brit.co
+*/
+
+if (!defined('WP_CLI') || !WP_CLI) {
+  return;
+}
+
+
+class Schema extends WP_CLI_Command {
+  /**
+   * Extract relevant data from a reflection method / function
+   * @return array Data about the refleciton
+   */
+  protected function get_reflection_info($reflection) {
+    $filename = $reflection->getFileName();
+    if(strpos($filename, ABSPATH) === 0) {
+        $filename = substr($filename, strlen(ABSPATH));
+    }
+    $filename .= ':' .  $reflection->getStartLine();
+        
+    if($reflection instanceof \ReflectionMethod) {
+      $name = $reflection->getDeclaringClass()->getName() . '\\' . $reflection->getName();
+    } else {
+      $name = $reflection->getName();
+    }
+    
+    return array(
+      'filename' => $filename,
+      'name' => $name
+    );
+  }
+  
+  public function upgrade($args, $assoc_args=array()) {
+    global $wp_filter;
+    
+    if(!array_key_exists('schema_upgrade', $wp_filter)) {
+        WP_CLI::error("No schema upgrade hooks found");
+        return;
+    }
+
+
+    // Wrap all the hooks in a function that logs execution time
+    foreach($wp_filter['schema_upgrade'] as $priority => &$sub_hooks) {
+      foreach($sub_hooks as $key => &$hook) {
+        $function = $hook['function'];
+        
+        if(is_array($function)) {
+          $reflection = new ReflectionMethod($function[0], $function[1]);
+        } else {
+          $reflection = new ReflectionFunction($function);
+        }
+        
+        $info = $this->get_reflection_info($reflection);
+        
+        $_this = $this;
+        $hook['function'] = function() use ($_this, $info, $function) {
+          $before = microtime(true);
+          WP_CLI::log(sprintf("Executing %s", $info['filename']));
+          WP_CLI::log(sprintf("├── Function name: %s", $info['name']));
+          
+          $result = call_user_func_array($function, func_get_args());
+          
+          if($result != false) {
+            WP_CLI::log(sprintf("├── Skipping, result returned false"));
+            $_this->log_step('skip', $reflection);
+          } else {
+            $after = microtime(true);
+            $diff = number_format($after - $before, 5);
+            WP_CLI::log(sprintf("├── Completed in %Fs", $diff));
+            $_this->log_step('success', $reflection);
+          }
+        };
+      }
+    }
+    
+    // Run all associated hooks
+    do_action('schema_upgrade');
+  }
+}
+
+WP_CLI::add_command('schema', __NAMESPACE__ . '\Schema');
